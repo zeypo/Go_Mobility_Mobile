06c4f4ea185cfa229575b86434a520ad4761c4b7
laurent gourmelon <laurentgourmelon@luckycart.com>
2014-09-01 01:30:52 +0200

    work in progress / inte liste

---
+++ assets/js/gmap.js
@@ -0,0 +1,10 @@
+
+    var mapOptions = {
+      zoom: 14,
+      center: 45.0733377,4.5001282,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      mapTypeControl:false
+    };
+
+    map = new google.maps.Map($('#map')[0], mapOptions);
